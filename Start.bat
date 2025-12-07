3654ff666b034200305d25da2b0df867549312e5
Alper <alperjk@gmail.com>
2025-12-08 02:25:50 +0300

    Programı çalıştıran .bat dosyası eklendi.

---
+++ Start.bat
@@ -0,0 +1,7 @@
+@echo off
+:: "pythonw" komutu konsol penceresi olmadan Python'ı çalıştırır.
+:: "start" komutu ise işlemi bat dosyasından ayırır.
+start "" pythonw main.py
+
+:: Bat dosyası görevini tamamlayıp hemen kapanır.
+exit
